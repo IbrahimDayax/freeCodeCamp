73b2b876d101d47a3d28b4eacb968814ad56e262
Ibrahim Dayax <ibrahim907@protonmail.com>
2021-12-23 00:06:35 +0800

    81% done wit Intermediate Algorithm Scripting

---
+++ JavaScript Algorithms and Data Structures/Intermediate Algorithm Scripting/steamroller.js
@@ -0,0 +1,21 @@
+const steamrollArray = arr => {
+    const flatArr = [].concat(...arr);
+    return flatArr.some(Array.isArray) ? steamrollArray(flatArr) : flatArr;
+}
+
+
+console.log(steamrollArray([1, [2],
+    [3, [
+        [4, [
+            [
+                [
+                    [
+                        [2, [
+                            [21]
+                        ]]
+                    ]
+                ]
+            ]
+        ]]
+    ]]
+]));
