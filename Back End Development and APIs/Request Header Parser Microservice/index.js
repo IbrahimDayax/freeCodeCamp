5193bb420a2c6a507043e2bdfa7ab11c92954ad8
Ibrahim Dayax <ibrahim907@protonmail.com>
2022-11-05 23:56:33 +0800

    finished Request Header Parser Microservice but Im having issues with freeCodeCamp validation

--- Back End Development and APIs/Request Header Parser Microservice/index.js
+++ Back End Development and APIs/Request Header Parser Microservice/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require("cors");
-const port = 3000;
-const filePath = __dirname + "/public/index.html";
-
 app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 app.use(express.static(__dirname + "/public")); // enable cors to allow remote testing by FCC
 
+const port = 3000;
+const filePath = __dirname + "/public/index.html";
 app.get("/", (req, res) => res.sendFile(filePath));
+
+
+const requestIP = require("request-ip");
+const ipMiddleware = (req, res, next) => {
+    requestIP.getClientIp(req);
+    next();
+}
+app.use(requestIP.mw());
+
+app.get("/api/whoami", (req, res) => {
+    const ipadress = req.clientIp;
+    const language = req.acceptsLanguages();
+    const software = req.get("User-Agent");
+    res.send({
+        ipadress: ipadress,
+        language:language[0],
+        software:software
+    });
+})
 
 
 
