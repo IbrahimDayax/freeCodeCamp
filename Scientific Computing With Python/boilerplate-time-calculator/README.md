9d09c7e1e8e2fbee06ef43dbe41f28ab6a2578fe
Ibrahim Dayax <ibrahim907@protonmail.com>
2023-01-09 20:16:59 +0800

    fixed all the errors, now everything is working perfectly

--- Scientific Computing With Python/boilerplate-time-calculator/README.md
+++ Scientific Computing With Python/boilerplate-time-calculator/README.md
@@ -1,3 +1,22 @@
-# Time Calculator
+# add_time
 
-This is the boilerplate for the Time Calculator project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/scientific-computing-with-python/scientific-computing-with-python-projects/time-calculator
+A Python function that takes a start time, a duration, and optionally a day of the week as inputs and returns the new time and day of the week after the specified duration has passed.
+
+## Inputs
+
+    start_time: a string in the format "hh:mm AM/PM" representing the starting time
+    duration: a string in the format "hh:mm" representing the duration of time to add to the start time
+    day_of_week: an optional string representing the day of the week (e.g. "Monday", "Tuesday", etc.)
+
+## Outputs
+
+    A string in the format "hh:mm AM/PM, day_of_week" or "hh:mm AM/PM, day_of_week (next day)" if the duration spans over the current day, or "hh:mm AM/PM, day_of_week (x days later)" if the duration spans over multiple days.
+
+## Examples
+
+```python
+add_time("9:00 AM", "1:30") => "10:30 AM"
+add_time("9:00 AM", "1:30", "Monday") => "10:30 AM, Monday"
+add_time("9:00 AM", "25:00", "Monday") => "10:00 AM, Tuesday (next day)"
+add_time("9:00 AM", "48:00", "Monday") => "09:00 AM, Wednesday (2 days later)"
+```
--- Scientific Computing With Python/boilerplate-time-calculator/time_calc.py
+++ Scientific Computing With Python/boilerplate-time-calculator/time_calc.py
@@ -21,9 +21,9 @@ def add_time(start_time, duration, day_of_week=None):
     if new_time.minutes >= 60:
         new_time.hours += 1
         new_time.minutes -= 60
-        # if the minutes are less than 10 add a 0 in front of them
-        if new_time.minutes < 10:
-            new_time.minutes = "0" + str(new_time.minutes)
+    # if the minutes are less than 10 add a 0 in front of them, eg to get 5:01 instead of 5:1
+    if new_time.minutes < 10:
+        new_time.minutes = "0" + str(new_time.minutes)
 
     # calculate the number of days that passed
     days_later = days_passed(new_time.hours, AM) 
@@ -31,18 +31,24 @@ def add_time(start_time, duration, day_of_week=None):
     time_of_day = am_or_pm(AM, new_time.hours)
       
     if day_of_week:
+        # convert the day_of_week string to lowercase then make the first letter uppercase
         day_of_week = day_of_week.lower().title()
+        # get the current index of the day of week before the time was added
         day_index = weekdays.index(day_of_week)
+        # calculate the actual time in the new day from the total time passed
         if new_time.hours > 24:
             new_time.hours = new_time.hours - days_later * 24
+        # make sure new time is in 12-hour format
         if new_time.hours > 12:
             new_time.hours -= 12
-        if day_index + days_later > 6:
-            day_index = (days_later + day_index) - 7
-            day_of_week = weekdays[day_index]
-        else:
-            day_index = days_later
-            day_of_week = weekdays[day_index]
+        if new_time.hours == 0:
+            new_time.hours = 12
+        # get rid of any negatives
+        if new_time.hours < 0:
+            new_time.hours *= -1
+        # iterate over the weekdays tuple circularly when looking for the new day of the week
+        day_index = (days_later + day_index) % len(weekdays)
+        day_of_week = weekdays[day_index]
             
         if days_later > 1:
             return new_time.__str__() + time_of_day + ", " + day_of_week + " (" + str(days_later) + " days later" + ")"
@@ -52,12 +58,18 @@ def add_time(start_time, duration, day_of_week=None):
             return new_time.__str__() + time_of_day + ", " + day_of_week
         
     else:
+        # calculate the actual time in the new day from the total time passed
         if new_time.hours > 24:
             new_time.hours = new_time.hours - days_later * 24
-        if new_time.hours == 0:
-            new_time.hours = 12
+        # make sure the new time is in 12-hour format
         if new_time.hours > 12:
             new_time.hours -= 12
+        if new_time.hours == 0:
+            new_time.hours = 12
+        # get rid of any negatives
+        if new_time.hours < 0:
+            new_time.hours *= -1
+            
         if days_later > 1:
             return new_time.__str__() + time_of_day + " (" + str(days_later) + " days later" + ")"
         elif days_later == 1:
@@ -65,5 +77,3 @@ def add_time(start_time, duration, day_of_week=None):
         else:
             return new_time.__str__() + time_of_day
         
-        
-print(add_time("2:59 AM", "24:00", "saturDay"))
